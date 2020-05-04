0cea3de2128abbbc6e67e087ef588fcaa38674c4
Samuel Pelsy <samuel.pelsymozimann@unistra.fr>
2020-05-04 20:02:14 +0100

    ADD: ajout clean

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -8,50 +8,30 @@ public class CharacterController2D : MonoBehaviour
     public bool isLeft = false;
     public float moveSpeed;
 
+    float jumpForce = 5f;
     public bool onWallJump = false;
 	void Update(){
         //saut
 		Jump();
 
-        if(onWallJump){
-            Debug.Log(gameObject.GetComponent<Rigidbody2D>().velocity.y);
-        }
         //ma vitesse en chute
         float velocityFall = 8f;
 
         //on défini les deplacement du joueur avec les appuis de touche
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         //si on est sur un mur a droite ou a gauche alors on reste accroché
-        if (!onWallJump && (isLeft && (movement.x < 0) || (isRight && (movement.x > 0)))){
-            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(gameObject.GetComponent<Rigidbody2D>().velocity.x,0f);
-        }
-        if(onWallJump){
-            Debug.Log(gameObject.GetComponent<Rigidbody2D>().velocity.y);
-        }
+        // if (!onWallJump && (isLeft && (movement.x < 0) || (isRight && (movement.x > 0)))){
+        //     gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(gameObject.GetComponent<Rigidbody2D>().velocity.x,0f);
+        // }
 
-        //max speed while falling
+        //vitesse de chute max
         //gameObject.GetComponent<Rigidbody2D>().velocity = Vector3.ClampMagnitude(gameObject.GetComponent<Rigidbody2D>().velocity, velocityFall);
         
-        //***** si le personnage arrive dans la limite basse du terrain il réapparait au dessus *****
-        //taille du monde
-        Vector3 wrld = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0.0f)); 
-        //taille du joueur
-        Vector3 sizePlayer = new Vector3(gameObject.GetComponent<BoxCollider2D>().size.x, gameObject.GetComponent<BoxCollider2D>().size.y, 0.0f);
-        //Si le personnage disparait en bas il réapparait en haut
-        if (gameObject.GetComponent<Rigidbody2D>().position.y <= -wrld.y- sizePlayer.y/2)
-        {
-            Vector3 deplacement = new Vector3(transform.position.x, wrld.y+ sizePlayer.y / 2, transform.position.z);
-            transform.position = deplacement;
-        }
-        //si monte retourne en bas
-        /*if (gameObject.GetComponent<Rigidbody2D>().position.y >= wrld.y)
-        {
-            Vector3 deplacement = new Vector3(transform.position.x, 0, transform.position.z);
-            transform.position = deplacement;
-        }*/
+        //***** si le personnage arrive dans la limite du terrain il réapparait de l'autre coté *****
+        PassVertical();
+        PassHorizontal();
 
         //si la vitesse est négative et qu'on saute alors
-        //on reprend la main du personnage
         if (gameObject.GetComponent<Rigidbody2D>().velocity.y <= 0 && onWallJump){
             onWallJump = false;
         }
@@ -63,17 +43,39 @@ public class CharacterController2D : MonoBehaviour
 
     }
 
+    void PassHorizontal(){
+        //si monte retourne en bas
+        /*if (gameObject.GetComponent<Rigidbody2D>().position.y >= wrld.y)
+        {
+            Vector3 deplacement = new Vector3(transform.position.x, 0, transform.position.z);
+            transform.position = deplacement;
+        }*/
+    }
+
+    void PassVertical(){
+        //taille du monde
+        Vector3 wrld = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0.0f)); 
+        //taille du joueur
+        Vector3 sizePlayer = new Vector3(gameObject.GetComponent<BoxCollider2D>().size.x, gameObject.GetComponent<BoxCollider2D>().size.y, 0.0f);
+        //Si le personnage disparait en bas il réapparait en haut
+        if (gameObject.GetComponent<Rigidbody2D>().position.y <= -wrld.y- sizePlayer.y/2)
+        {
+            Vector3 deplacement = new Vector3(transform.position.x, wrld.y+ sizePlayer.y / 2, transform.position.z);
+            transform.position = deplacement;
+        }
+    }
+
 	void Jump(){
 		if(Input.GetButtonDown("Jump") && isGrounded){
-			gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+			gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
 		}
-        if (Input.GetButtonDown("Jump") && (isRight || isLeft))
+        else if (Input.GetButtonDown("Jump") && (isRight || isLeft) && !isGrounded)
         {
             if(isRight){
-                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-2f, 5f), ForceMode2D.Impulse);
+                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-2f, jumpForce), ForceMode2D.Impulse);
             }
             else if(isLeft){
-                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(2f, 5f), ForceMode2D.Impulse);
+                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(2f, jumpForce), ForceMode2D.Impulse);
             }
             onWallJump = true;
         }
